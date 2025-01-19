84ef6dd881977ddbc1a11106cbbe47c3c2f6e381
Koch2333 <Koch233333@gmail.com>
2025-01-19 23:40:48 +0800

    New personal Website initial commit. use Vue 3, UnoCSS and NaiveUI.

---
+++ Koch2333/src/components/Contact.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { NIcon, NSpace } from 'naive-ui'
+import { Github, Telegram, Twitter } from '@vicons/fa'
+</script>
+
+<template>
+  <n-space justify="start">
+    <n-icon size="32">
+      <a href="https://github.com/Koch2333" target="_blank"><Github />
+      </a>
+      <a href="https://t.me/@Koch2333" target="_blank"><Telegram />
+      </a>
+      <a href="https://x.com/koch2333" target="_blank">
+        <Twitter />
+      </a>
+    </n-icon>
+  </n-space>
+</template>
+
+<style scoped>
+
+</style>
