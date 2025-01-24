684c5569200451aa33ac14f7d52c0c75fd6f1c4f
Koch2333 <Koch233333@gmail.com>
2025-01-24 21:36:56 +0800

    Changed avatar's source.

--- Koch2333/src/components/HelloWorld.vue
+++ Koch2333/src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps({
 <template>
   <div class="md:flex">
     <a href="https://x.com/to0699/status/1686300087507664897?t=rBIo0MhK21JLJluI40SQlg&s=19" target="_blank">
-      <n-avatar src="https://avatars.githubusercontent.com/u/54238405?v=4" class="size-48 " round>
+      <n-avatar src="https://koch2333.cn/avatar.jpg" class="size-48 " round>
       </n-avatar></a>
   <div class="m-1 text-left leading-4 md:shrink-0">
         <h2>{{msg}}</h2>
